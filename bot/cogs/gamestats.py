2cdfd304c1863b09a87a570c3272fadc9c15980d
Rohith04MVK <67266075+Rohith04MVK@users.noreply.github.com>
2020-09-23 17:20:35 +0530

    uuid finder for Minecraft

--- bot/cogs/gamestats.py
+++ bot/cogs/gamestats.py
@@ -7,6 +7,13 @@ class GameStats(Cog):
     def __init__(self, bot: Bot) -> None:
         self.bot = bot
 
+        
+@command()
+async def minecraft(self, ctx: Context, username: str) -> None:
+  async with self.bot.session.get(f"https://api.mojang.com/users/profiles/minecraft/{username}", headers={'Accept': 'application/json'}) as session:
+    uuid = await session.json()
+  await ctx.send(uuid["id"])        
+        
 
 def setup(bot: Bot) -> None:
     bot.add_cog(GameStats(bot))
