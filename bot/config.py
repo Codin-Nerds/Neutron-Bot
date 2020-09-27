50e7c69c410d88bf05c33988960ba40b4bd38d9f
ItsDrike <itsdrikeofficial@gmail.com>
2020-09-27 11:20:52 +0200

    Add database manager file

--- bot/config.py
+++ bot/config.py
@@ -4,9 +4,15 @@ import os
 # Developer Mode Settings:
 DEV_MODE = True
 
-# Developer
-creator = "janaSunrise"
-devs = [711194921683648523]
+# Database
+DATABASE = {
+    "host": os.getenv("DATABASE_HOST", "127.0.0.1"),
+    "database": os.getenv("DATABASE_NAME"),
+    "user": os.getenv("DATABASE_USER"),
+    "password": os.getenv("DATABASE_PASSWORD"),
+    "min_size": int(os.getenv("POOL_MIN", "20")),
+    "max_size": int(os.getenv("POOL_MAX", "100")),
+}
 
 # Prefix Settings
 COMMAND_PREFIX = os.getenv("COMMAND_PREFIX", ">>")
