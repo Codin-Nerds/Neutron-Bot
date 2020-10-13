7c73467539ca46a26d207b770d17cc90718ceb99
ItsDrike <itsdrikeofficial@gmail.com>
2020-10-14 01:48:06 +0200

    Set error handle order

--- bot/cogs/error_handler.py
+++ bot/cogs/error_handler.py
@@ -24,8 +24,8 @@ class ErrorHandler(Cog):
 
     async def send_unhandled_embed(self, ctx: Context, exception: errors.CommandError) -> None:
         logger.debug(
-            f"Exception {exception.__class__.__name__}: {exception} has occurred in "
-            f" command {ctx.command} invoked by {ctx.author.id} on {ctx.guild.id}"
+            f"Exception {exception.__class__.__name__}: {exception} has occurred from "
+            f"message {ctx.message.content} invoked by {ctx.author.id} on {ctx.guild.id}"
         )
         await self._send_error_embed(
             ctx,
@@ -45,7 +45,35 @@ class ErrorHandler(Cog):
 
     @Cog.listener()
     async def on_command_error(self, ctx: Context, exception: errors.CommandError) -> None:
+        """
+        Handle exceptions which occurred while running some command.
+
+        The error handle order is as follows:
+        1. `UserInputError`: references `handle_user_input_error`
+        2. `CommandNotFound`: references `handle_command_not_found`
+        3. `CheckFailure`: references `handle_command_not_found`
+        4. Otherwise, send unhandled error embed with `send_unhandled_embed`
+        """
+        if isinstance(exception, errors.UserInputError):
+            await self.handle_user_input_error(ctx, exception)
+            return
+        elif isinstance(exception, errors.CommandNotFound):
+            await self.handle_command_not_found(ctx, exception)
+            return
+        elif isinstance(exception, errors.CheckFailure):
+            await self.handle_check_failure(ctx, exception)
+            return
+
         await self.send_unhandled_embed(ctx, exception)
+
+    async def handle_user_input_error(self, ctx: Context, exception: errors.UserInputError) -> None:
+        pass
+
+    async def handle_command_not_found(self, ctx: Context, exception: errors.CommandNotFound) -> None:
+        pass
+
+    async def handle_check_failure(self, ctx: Context, exception: errors.CheckFailure) -> None:
+        pass
 
 
 def setup(bot: Bot) -> None:
