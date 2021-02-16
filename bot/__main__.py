8fabfd8f631d1afd484734f275521ac1c61ef632
ItsDrike <itsdrikeofficial@gmail.com>
2021-02-16 23:42:09 +0100

    Require member intent (needed for join logs)

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,6 +1,6 @@
 import os
 
-from discord import Game
+from discord import Game, Intents
 
 from bot import config
 from bot.core.bot import Bot
@@ -8,10 +8,14 @@ from bot.core.bot import Bot
 TOKEN = os.getenv("BOT_TOKEN")
 PREFIX = config.COMMAND_PREFIX
 
+intents = Intents.default()
+intents.members = True
+
 bot = Bot(
     command_prefix=PREFIX,
     activity=Game(name=f"Ping me using {PREFIX}help"),
     case_insensitive=True,
+    intents=intents
 )
 
 
