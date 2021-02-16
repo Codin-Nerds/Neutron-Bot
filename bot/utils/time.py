3777c567e602030a059da6ad4db01fa0b743cdc9
ItsDrike <itsdrikeofficial@gmail.com>
2021-02-16 23:49:15 +0100

    Add max_units limitation

--- bot/utils/time.py
+++ bot/utils/time.py
@@ -4,7 +4,7 @@ from datetime import datetime, timedelta
 from dateutil.relativedelta import relativedelta
 
 
-def stringify_reldelta(rel_delta: relativedelta, min_unit: str = "seconds") -> str:
+def stringify_reldelta(rel_delta: relativedelta, min_unit: str = "seconds", max_units: int = 8) -> str:
     """
     Convert `dateutil.relativedelta.relativedelta` into a readable string
 
@@ -23,6 +23,10 @@ def stringify_reldelta(rel_delta: relativedelta, min_unit: str = "seconds") -> s
     `1 year 2 months 2 weeks 5 days 4 hours 2 minutes and 1 second`
     you'd get:
     `1 year 2 months 2 weeks and 5 days`
+
+    `max_units` is the maximum amount of units to be used.
+    If the produced string would go over this amount, smaller
+    units will be cut to fit into this number.
     """
     rel_delta = rel_delta.normalized()
     time_dict = {
@@ -40,9 +44,15 @@ def stringify_reldelta(rel_delta: relativedelta, min_unit: str = "seconds") -> s
     time_list = []
 
     for unit, value in time_dict.items():
+        # Stop early and don't parse smaller units
+        # if we already hit the max allowed amount of units
+        if len(time_list) == max_units:
+            break
+
         if value:
             time_list.append(f"{int(value)} {unit if value != 1 else unit[:-1]}")
 
+        # Stop if we hit the minimal unit
         if unit == min_unit:
             break
 
@@ -57,7 +67,7 @@ def stringify_reldelta(rel_delta: relativedelta, min_unit: str = "seconds") -> s
     return stringified_time
 
 
-def stringify_timedelta(time_delta: timedelta, min_unit: str = "seconds") -> str:
+def stringify_timedelta(time_delta: timedelta, min_unit: str = "seconds", max_units: int = 8) -> str:
     """
     Convert `datetime.timedelta` into a readable string
 
@@ -76,13 +86,17 @@ def stringify_timedelta(time_delta: timedelta, min_unit: str = "seconds") -> str
     `1 year 2 months 2 weeks 5 days 4 hours 2 minutes and 1 second`
     you'd get:
     `1 year 2 months 2 weeks and 5 days`
+
+    `max_units` is the maximum amount of units to be used.
+    If the produced string would go over this amount, smaller
+    units will be cut to fit into this number.
     """
     now = datetime.now()
     rel_delta = relativedelta(now + time_delta, now)
-    return stringify_reldelta(rel_delta, min_unit=min_unit)
+    return stringify_reldelta(rel_delta, min_unit=min_unit, max_units=max_units)
 
 
-def stringify_duration(duration: t.Union[int, float], min_unit: str = "seconds") -> str:
+def stringify_duration(duration: t.Union[int, float], min_unit: str = "seconds", max_units: int = 8) -> str:
     """
     Convert `duration` in seconds into a readable time string
 
@@ -101,6 +115,10 @@ def stringify_duration(duration: t.Union[int, float], min_unit: str = "seconds")
     `1 year 2 months 2 weeks 5 days 4 hours 2 minutes and 1 second`
     you'd get:
     `1 year 2 months 2 weeks and 5 days`
+
+    `max_units` is the maximum amount of units to be used.
+    If the produced string would go over this amount, smaller
+    units will be cut to fit into this number.
     """
     if isinstance(duration, float):
         if duration == float("inf"):
@@ -108,10 +126,10 @@ def stringify_duration(duration: t.Union[int, float], min_unit: str = "seconds")
 
     now = datetime.now()
     rel_delta = relativedelta(now + timedelta(seconds=duration), now)
-    return stringify_reldelta(rel_delta, min_unit=min_unit)
+    return stringify_reldelta(rel_delta, min_unit=min_unit, max_units=max_units)
 
 
-def time_elapsed(_from: datetime, to: t.Optional[datetime] = None, min_unit: str = "seconds") -> str:
+def time_elapsed(_from: datetime, to: t.Optional[datetime] = None, min_unit: str = "seconds", max_units: int = 8) -> str:
     """
     Returns how much time has elapsed in a readable string
     when no `to` value is specified, current time is assumed
@@ -131,10 +149,14 @@ def time_elapsed(_from: datetime, to: t.Optional[datetime] = None, min_unit: str
     `1 year 2 months 2 weeks 5 days 4 hours 2 minutes and 1 second ago`
     you'd get:
     `1 year 2 months 2 weeks and 5 days ago`
+
+    `max_units` is the maximum amount of units to be used.
+    If the produced string would go over this amount, smaller
+    units will be cut to fit into this number.
     """
     if not to:
         to = datetime.utcnow()
 
     rel_delta = relativedelta(to, _from)
-    stringified_time = stringify_reldelta(rel_delta, min_unit=min_unit)
+    stringified_time = stringify_reldelta(rel_delta, min_unit=min_unit, max_units=max_units)
     return f"{stringified_time} ago."
